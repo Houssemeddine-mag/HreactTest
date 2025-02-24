0985d85dedb1c7fb687fb0d8abe05fd3c92e89df
Houssemeddine-mag <houssem.magra@univ-constantine2.dz>
2025-02-24 15:10:16 +0100

    changes

--- src/button.jsx
+++ src/button.jsx
@@ -2,8 +2,25 @@ import "./Card.css";
 
 export default function Button(props) {
   return (
-    <div>
-      <button className="Button">{props.name}</button>
-    </div>
+    <button id="btn" className="Button">
+      {props.name}
+    </button>
   );
 }
+
+// import { useState } from "react";
+// import "./Card.css";
+
+// export default function Button(props) {
+//   const [buttonName, setButtonName] = useState(props.name); // Initial name from props
+
+//   const handleClick = () => {
+//     setButtonName((prevName) => (prevName === props.name ? "houssem" : props.name));
+//   };
+
+//   return (
+//     <button id="btn" className="Button" onClick={handleClick}>
+//       {buttonName}
+//     </button>
+//   );
+// }
